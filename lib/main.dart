cc6f5a1e01ae6577a907611709debb1f5333b6d4
Seth <60856766+sethupavan12@users.noreply.github.com>
2021-06-15 19:15:56 +0100

    commit 2

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cource/product_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: (Text('EasyList')),
+            ),
+            body: ProductManager('Food bester')));
+  }
+}
